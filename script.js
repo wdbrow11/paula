2267e633f5742f5c9a0aeb90a3d2af32d4eea267
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:59:02 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "Your  positivity and personality is so infectious.",
+"Your excitement everyday to learn and how much you love shawn mendes",
+"you are super positive and helpful to others",
+"How you light up the room. You are always a part of such creative conversations!",
+"How you help me. Also you're really funny and I like being around you.",
+"your positivity and persistence, especially when you work with others",
+"You are so funny and I love your personality.",
+"your cheerfulness, positivity, love for exploration, and how easy it is to talk to you. I love how I'm always laughing and smiling when I'm with you. I love how you always have funny stories and it gives me something to look forward to all the time.",
+"that you always have a smile on your face.",
+"How fun you are to talk to!",
+"Your kindness and enthusiasm. I love your unrelenting determination and creative choices!!!",
+"Your enthusiasm over the things you love!",
+"how funny and sweet you are. I love talking to you in the morning. You made me feel so much more welcome, and I really enjoy talking to you and going to get candy and coffee with you.",
+"Your energy. It is so contagious and your positivity towards everything is so admirable."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
